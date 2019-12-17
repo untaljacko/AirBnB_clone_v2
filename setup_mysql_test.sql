9abf100d838c8d71797f84856f1b692b29561928
DiegoRmsR <Diegoramos.mt@gmail.com>
2019-12-17 22:18:17 +0000

    [Feature] prepares a MySQL server for the projec

---
+++ setup_mysql_test.sql
@@ -0,0 +1,9 @@
+-- Create a new database
+-- Create a new User
+-- All privileges for the new user
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost';
+SET PASSWORD FOR 'hbnb_test'@'localhost' = 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
