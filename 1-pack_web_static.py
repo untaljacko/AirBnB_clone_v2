98fbee9efbac6c877e8b2c8872cba6fea81e5dcc
untaljacko <untaljacko@gmail.com>
2020-01-10 23:59:13 +0000

    [fix] task 1, pep8

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -9,6 +9,7 @@ from fabric.api import local
 def do_pack():
     """ doc do_pack """
     local("mkdir -p versions")
-    new_pack = "versions/web_static_{}.tgz".format(datetime.now().strftime("%Y%m%d%H%M%S"))
+    new_pack = "versions/web_static_{}.tgz".format(
+        datetime.now().strftime("%Y%m%d%H%M%S"))
     def_pack = local("tar -cvzf {} web_static".format(new_pack))
     return def_pack
