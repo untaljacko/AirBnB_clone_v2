c4b38a7679370aaec5da0fa051cbddc4ef094ccc
untaljacko <untaljacko@gmail.com>
2020-01-11 00:44:11 +0000

    [fix] task 2, pep8

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -18,6 +18,7 @@ def do_pack():
     def_pack = local("tar -cvzf {} web_static".format(new_pack))
     return def_pack
 
+
 def do_deploy(archive_path):
     """ doc do_deploy """
     if exists(archive_path):
@@ -25,14 +26,16 @@ def do_deploy(archive_path):
         pack_file = archive_path.split("/")[1].split(".")[0]
         remote_path = "/data/web_static/releases/{}".format(pack_file)
         run("mkdir {}".format(remote_path))
-        run("tar -zxvf /tmp/{}.tgz --directory {}/".format(pack_file, remote_path))
+        run("tar -zxvf /tmp/{}.tgz --directory {}/".format(
+            pack_file, remote_path))
         run("rm /tmp/{}".format(archive_path.split("/")[1]))
         run("rm /data/web_static/current")
         run("ln -sf /data/web_static/releases/{}\
             /data/web_static/current".format(pack_file))
         run("mv /data/web_static/releases/{}/web_static/*\
             /data/web_static/current/".format(pack_file))
-        run("rm -rf /data/web_static/releases/{}/web_static/".format(pack_file))
+        run("rm -rf /data/web_static/releases/{}/web_static/".format(
+            pack_file))
         return True
     else:
         return False
