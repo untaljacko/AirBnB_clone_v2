e3e530ed434acc524a6d09b009f079bfcef14a43
untaljacko <untaljacko@gmail.com>
2020-01-22 16:21:23 +0000

    [new] task 4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+""" A script that starts a Flask web application """
+
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """ return the message Hello HBNB! """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ return the message HBNB """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """ display C and the content of the text variable and replace underscore
+    with space """
+    return 'C %s' % text.replace('_', ' ')
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text="is cool"):
+    """ display Python and the content of the text variable and replace underscore
+    with space """
+    return 'Python %s' % text.replace('_', ' ')
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """ display n is a number """
+    return "%d is a number" % n
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
