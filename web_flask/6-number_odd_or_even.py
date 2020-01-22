5dacceb6d58fb9fb9b31f797b3146410546f5fc6
untaljacko <untaljacko@gmail.com>
2020-01-22 17:17:32 +0000

    [fix] task 6 typo with an extra " in docstring

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -47,7 +47,7 @@ def number_template(n):
 
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def number_odd_or_even(n):
-    """ display a HTML page and if n is odd or even """"
+    """ display a HTML page and if n is odd or even """
     return render_template('6-number_odd_or_even.html', number=n)
 
 
